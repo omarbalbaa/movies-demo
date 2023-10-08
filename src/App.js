ada7720e6790e7764a5d6bd33dc27899b4f8caed
Balbaa <omareidbalbaa@gmail.com>
2023-10-08 17:12:34 +0300

    Added search with enter button feature

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ const API_URL = "http://www.omdbapi.com?apikey=64d0bd43";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState ("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search)
     setMovies(data.Search);
   };
+
   useEffect(() => {
-    searchMovies('search');
+    searchMovies("search");
   }, []);
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      searchMovies(searchTerm);
+    }
+  };
   return (
     <div className="app">
       <h1>MovieLand</h1>
@@ -28,13 +33,15 @@ const App = () => {
         <input
           placeholder="Search for movies"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value) }
+          onChange={(event) => setSearchTerm(event.target.value)}
+          onKeyDown={handleKeyDown}
         />
 
         <img
-        src={SearchIcon}
-        alt="search"
-        onClick={() => searchMovies(searchTerm)} />
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
       {movies?.length > 0 ? (
