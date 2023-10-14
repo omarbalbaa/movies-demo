b69de488597f519835c9e53de498642d6934836b
Balbaa <omareidbalbaa@gmail.com>
2023-10-14 15:20:13 +0300

    changed api request to a secure protocol

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=64d0bd43";
+const API_URL = "https://www.omdbapi.com?apikey=64d0bd43";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
